37c62c9b495db5b282f5ca760d03d295af685a6a
William de Aguiar Campos <138343439+GDCom62@users.noreply.github.com>
2026-07-05 15:30:48 -0300

    Change Supabase URL and key
    Updated Supabase connection details with new URL and key.

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from supabase import create_client, Client
 st.set_page_config(page_title="Plano de Ação Lavo e Levo", layout="wide")
 
 # CONEXÃO DIRETA COM O SUPABASE
-SUPABASE_URL = "https://supabase.co" 
-SUPABASE_KEY = "sua-chave-anonima-longa-real-aqui"
+SUPABASE_URL = "https://otlzkpjlzorxdhagqksf.supabase.co" 
+SUPABASE_KEY = "sb_publishable_UtC2lBc6OwE0ZrWFpL7U9g_VuTjjjSw"
 
 def get_supabase_client() -> Client:
     return create_client(SUPABASE_URL, SUPABASE_KEY)
