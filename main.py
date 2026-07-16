ee4bd6e3f32980e83654e294da7b4893432076a5
William de Aguiar Campos <138343439+GDCom62@users.noreply.github.com>
2026-07-16 19:25:38 -0300

    Change Supabase URL to a generic placeholder
    Updated Supabase URL to a more generic placeholder.

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ from supabase import create_client, Client
 st.set_page_config(page_title="Plano de Ação - Administrativo", layout="wide")
 
 # CONEXÃO DIRETA COM O SUPABASE
-# Lembre-se de preencher com a URL e KEY corretas do seu projeto
-SUPABASE_URL = "https://otlzkpjlzorxdhagqksf.supabase.co" 
+SUPABASE_URL = "https://supabase.co" 
 SUPABASE_KEY = "sb_publishable_UtC2lBc6OwE0ZrWFpL7U9g_VuTjjjSw"
 
 def get_supabase_client() -> Client:
@@ -249,3 +248,5 @@ if acoes:
     col_sel, col_btn_ed, col_btn_ex = st.columns(3)
     
     with col_sel:
+        ids_disponiveis = [a for a in df_filtrado['id_acao']]
+        if ids_disponiveis:
